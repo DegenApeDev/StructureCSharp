3799cb4cbd53c78b49e7ee2a1601b133b1b34bfb
linuzo <linuzo@LINUZO-LAPTOP>
2016-07-02 10:11:59 -0600

    Add project files.

---
+++ StructureCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVariable;
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
